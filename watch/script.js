d06803c8f16a2d3e3110d8a3c576db795755f687
Przemysław Pierzchałka <przemekpierzchalka@gmail.com>
2018-12-13 01:21:27 +0100

    Update script.js

--- watch/script.js
+++ watch/script.js
@@ -1,9 +1,13 @@
-let date = {
+'use strict';
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var date = {
   hours: '',
   mintes: ''
 };
 
-const words = {
+var words = {
   sentenceParts: {
     it: document.getElementById('it'),
     is: document.getElementById('is'),
@@ -22,23 +26,10 @@ const words = {
     past: document.getElementById('past'),
     equal: document.getElementById('oclock-h')
   },
-  hours: [
-    document.getElementById('one-h'),
-    document.getElementById('two-h'),
-    document.getElementById('three-h'),
-    document.getElementById('four-h'),
-    document.getElementById('five-h'),
-    document.getElementById('six-h'),
-    document.getElementById('seven-h'),
-    document.getElementById('eight-h'),
-    document.getElementById('nine-h'),
-    document.getElementById('ten-h'),
-    document.getElementById('eleven-h'),
-    document.getElementById('twelve-h')
-  ]
+  hours: [document.getElementById('one-h'), document.getElementById('two-h'), document.getElementById('three-h'), document.getElementById('four-h'), document.getElementById('five-h'), document.getElementById('six-h'), document.getElementById('seven-h'), document.getElementById('eight-h'), document.getElementById('nine-h'), document.getElementById('ten-h'), document.getElementById('eleven-h'), document.getElementById('twelve-h')]
 };
 
-const setMinutes = function() {
+var setMinutes = function setMinutes() {
   if (date.minutes <= 5) {
     // 05 past
     addActiveClass(words.minutes.five);
@@ -75,46 +66,77 @@ const setMinutes = function() {
     // 5 to
     addActiveClass(words.minutes.five);
   }
-}
+};
 
-const setHours = function() {
-  addActiveClass(words.hours[date.hours === 12 ? (date.pastHalf ? 0 : 11) : (date.pastHalf ? date.hours : date.hours - 1)]);
-  addActiveClass(date.minutes <= 55 ? (date.pastHalf ? words.dividers.to : words.dividers.past) : words.dividers.equal);
-}
+var setHours = function setHours() {
+  addActiveClass(words.hours[date.hours === 12 ? date.pastHalf ? 0 : 11 : date.pastHalf ? date.hours : date.hours - 1]);
+  addActiveClass(date.minutes <= 55 ? date.pastHalf ? words.dividers.to : words.dividers.past : words.dividers.equal);
+};
 
-const setAm = function() {
+var setAm = function setAm() {
   date.am ? addActiveClass(words.sentenceParts.am) : addActiveClass(words.sentenceParts.pm);
-}
+};
 
-const addActiveClass = function(elem) {
+var addActiveClass = function addActiveClass(elem) {
   elem.classList.add('active');
-}
+};
 
-const activateDisplay = function() {
+var activateDisplay = function activateDisplay() {
   setAm();
   setHours();
   setMinutes();
-}
+};
 
-const removeActiveClass = function() {
-  const activated = Array.from(document.getElementsByClassName('active'));
-  activated.forEach(elem => elem.classList.remove('active'));
-}
+var removeActiveClass = function removeActiveClass() {
+  var activated = Array.from(document.getElementsByClassName('active'));
+  activated.forEach(function (elem) {
+    return elem.classList.remove('active');
+  });
+};
 
-const runClock = function() {
+var runClock = function runClock() {
   removeActiveClass();
   activateDisplay();
-}
-
-const setDate = async function() {
-  date = {
-    hours: new Date().getHours() % 12 || 12,
-    minutes: new Date().getMinutes() + 2,
-    am: new Date().getHours() <= 12 ? true : false,
-    pastHalf: new Date().getMinutes() > 30 ? true : false
-  };
-  runClock();
 };
+
+var setDate = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var fetched, fetchedJson;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return fetch('http://worldclockapi.com/api/json/cet/now');
+
+          case 2:
+            fetched = _context.sent;
+            _context.next = 5;
+            return fetched.json();
+
+          case 5:
+            fetchedJson = _context.sent;
+
+            date = {
+              hours: new Date().getHours() % 12 || 12,
+              minutes: new Date().getMinutes() + 2,
+              am: new Date().getHours() <= 12 ? true : false,
+              pastHalf: new Date().getMinutes() > 30 ? true : false
+            };
+            runClock();
+
+          case 8:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function setDate() {
+    return _ref.apply(this, arguments);
+  };
+}();
 
 window.onload = setDate;
 setInterval(setDate, 30000);
