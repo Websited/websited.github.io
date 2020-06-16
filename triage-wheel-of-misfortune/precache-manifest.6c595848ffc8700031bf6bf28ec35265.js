f35270844586526bb5834d54180536f927240b63
Przemyslaw Pierzchalka <p.pierzchalka@vistex.com>
2020-06-16 23:34:33 +0200

    add wheel of misfortune

---
+++ triage-wheel-of-misfortune/precache-manifest.6c595848ffc8700031bf6bf28ec35265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cf933747af08f88e61b24236194c8f",
+    "url": "/triage-wheel-of-misfortune/index.html"
+  },
+  {
+    "revision": "671e623e481250741ba2",
+    "url": "/triage-wheel-of-misfortune/static/css/main.df8a443a.chunk.css"
+  },
+  {
+    "revision": "1138a3713660f7a1b48f",
+    "url": "/triage-wheel-of-misfortune/static/js/2.c9bfad0c.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/triage-wheel-of-misfortune/static/js/2.c9bfad0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671e623e481250741ba2",
+    "url": "/triage-wheel-of-misfortune/static/js/main.f59e0c57.chunk.js"
+  },
+  {
+    "revision": "facf7b108dbff459afeb",
+    "url": "/triage-wheel-of-misfortune/static/js/runtime-main.38a167ce.js"
+  }
+]);
